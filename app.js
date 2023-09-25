547d29b8ec48d1be1337cd20ddc694403c7dba51
KJY <rlawldbs1007@gmail.com>
2023-09-25 15:52:22 +0900

    프론트연결성공,jwt,cors추가-jy

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+const jwt = require('jsonwebtoken'); //toketn 생성모듈
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+const cors = require('cors');
 //경로 설정
 const path = require('path');
 
 // Body Parser 미들웨어 설정 - 파싱위함
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
 // MySQL 연결 모듈 가져오기
 const mysqlConnection = require('./dbconnection');
@@ -47,18 +52,26 @@ app.post('/login', (req, res) => {
             console.log(results);
             console.error('MySQL query error: ', err);
             res.status(500).json({ error: 'Internal Server Error' });
-            res.send('로그인 실패. 아이디 또는 비밀번호가 올바르지 않습니다.');
+            
+            //res.send('로그인 실패. 아이디 또는 비밀번호가 올바르지 않습니다.');
         } else {
             const user = results;
             console.log(user);
             if(user.length === 0){
                 console.log('로그인 실패 - 해당정보 사용자없음');
                 res.sendFile(path.join(__dirname + '/tempfront', 'login.html'));
-            }
-            console.log('유저정보 조회 성공');
+            }else{
+                console.log('유저 정보 조회 성공');
 
-            // 로그인이 성공하면 다음 작업을 수행 (예: 세션 설정, 페이지 이동)
-            res.sendFile(path.join(__dirname + '/tempfront', 'main.html'));
+                // 사용자 정보를 기반으로 JWT 생성
+                const token = jwt.sign({ userId: user.id }, 'your-secret-key', {
+                  expiresIn: '1h' // 토큰 만료 시간 설정 (예: 1시간)
+                });
+        
+                // 토큰을 클라이언트로 반환
+                res.json({ token });
+                //res.sendFile(path.join(__dirname + '/tempfront', 'main.html'));
+            }
         }
     });
 });
