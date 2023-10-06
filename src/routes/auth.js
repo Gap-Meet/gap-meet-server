6eaeb51c11f091c0ed8edc4dc9ce40211532e612
KJY <rlawldbs1007@gmail.com>
2023-10-07 03:16:18 +0900

    주석추가 - jy

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,17 @@
 import express from "express";
+
+// 각 API 엔드포인트에 대한 컨트롤러 함수 import(해당 엔드포인트에 대한 요청을 처리)
 import join from "../controller/auth/join.js"
 import resign from "../controller/auth/resign.js"
 import login from "../controller/auth/login.js"
 
 export const router = express.Router();
 
+
+/*
+    /api/user 경로의 요청을 처리하는 라우터를 정의
+    API 엔드포인트를 설정
+*/
 
 //POST /api/user/login
 router.post('/login', login);
