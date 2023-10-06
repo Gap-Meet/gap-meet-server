24a6db8d6e91a6cd87d50d8e65d8cdb8866636c3
KJY <rlawldbs1007@gmail.com>
2023-10-07 06:19:43 +0900

    주석추가 및 변수이름수정 -jy

--- src/controller/auth/resign.js
+++ src/controller/auth/resign.js
@@ -2,61 +2,60 @@ import pool from "../../config/database.js";
 import { remove } from "../../utils/cache.js";
 import { verify } from "../../utils/authjwt.js";
 import { delete_user } from "../../db/auth/userDao.js"; // 사용자 삭제 함수
-import { auth_user } from '../../db/auth/loginDao.js';
-import bcrypt from 'bcrypt';
+import { check_user } from "../../db/auth/loginDao.js";
+import bcrypt from "bcrypt";
 
 export const resign = async (req, res) => {
-    // 요청에서 사용자 ID를 추출합니다.
-    const { password } = req.body;
-    const conn = await pool.getConnection();
+  // 요청에서 사용자 ID를 추출합니다.
+  const { password } = req.body;
+  const conn = await pool.getConnection();
 
-    try {
-        // req.headers.authorization로부터 토큰 추출
-        const authorizationHeader = req.headers.authorization;
-        const token = authorizationHeader.split(' ')[1]; // 'Bearer' 토큰을 제외하고 토큰 추출
-        
-        console.log(token + "......."); //디버깅
-        const user_id = verify(token);
+  try {
+    // req.headers.authorization로부터 토큰 추출
+    const authorizationHeader = req.headers.authorization;
+    const token = authorizationHeader.split(" ")[1]; // 'Bearer' 토큰을 제외하고 토큰 추출
 
-        // 디버깅
-        console.log(user_id + "!!!!!!!!!!!!!!!!!!!!!!!");
-        
-        // 사용자명으로 데이터베이스에서 사용자 정보를 가져옵니다.
-        const user = await auth_user(conn, user_id);
-        console.log(user[0][0].password);
+    console.log(token + "......."); //디버깅
+    const user_id = verify(token);
 
-        // 비밀번호를 비교하여 일치하는지 확인합니다.
-        const match = await bcrypt.compare(password, user[0][0].password);
+    // 디버깅
+    console.log(user_id + "!!!!!!!!!!!!!!!!!!!!!!!");
 
-        if (!match) {
-            // 비밀번호가 일치하지 않는경우
-            return res.status(402).send({
-                ok: false,
-                msg: "비밀번호가 일치하지 않습니다."
-            });
-        }
+    // 사용자명으로 데이터베이스에서 사용자 정보를 가져옵니다.
+    const user = await check_user(conn, user_id);
+    console.log(user[0][0].password);
 
-        
-        // 데이터베이스에서 사용자 삭제 쿼리 실행
-        //await conn.query(delete_user(conn, user_id));
-        await conn.query('DELETE FROM users WHERE user_id = ?', [user_id]);
-        conn.release();
+    // 비밀번호를 비교하여 일치하는지 확인합니다.
+    const match = await bcrypt.compare(password, user[0][0].password);
 
-        // 캐시에서 리프레시 토큰을 삭제합니다.
-        remove(user_id);
-
-        // 성공 상태 코드(200)와 메시지를 응답으로 보냅니다.
-        res.status(200).send({
-            ok: true,
-            msg: "Account deleted successfully"
-        });
-    } catch (err) {
-        // 오류 처리 블록: 오류 메시지와 함께 오류 응답을 보냅니다.
-        res.status(500).send({
-            ok: false,
-            msg: err.message + "ㅜㅜ"
-        });
+    if (!match) {
+      // 비밀번호가 일치하지 않는경우
+      return res.status(402).send({
+        ok: false,
+        msg: "비밀번호가 일치하지 않습니다.",
+      });
     }
+
+    // 데이터베이스에서 사용자 삭제 쿼리 실행
+    //await conn.query(delete_user(conn, user_id));
+    await conn.query("DELETE FROM users WHERE user_id = ?", [user_id]);
+    conn.release();
+
+    // 캐시에서 리프레시 토큰을 삭제합니다.
+    remove(user_id);
+
+    // 성공 상태 코드(200)와 메시지를 응답으로 보냅니다.
+    res.status(200).send({
+      ok: true,
+      msg: "Account deleted successfully",
+    });
+  } catch (err) {
+    // 오류 처리 블록: 오류 메시지와 함께 오류 응답을 보냅니다.
+    res.status(500).send({
+      ok: false,
+      msg: err.message + "ㅜㅜ",
+    });
+  }
 };
 
 export default resign;
--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,28 +1,30 @@
 import redis from "redis";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-
 
 //Redis 연결
 export const redisClient = redis.createClient({
+  /*
+    Redis 클라이언트 객체 생성 :
+      Redis 서버에 연결하기 위한 URL을 지정
+      legacyMode 옵션을 활성화
+  */
   url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}/0`,
-  legacyMode: true, 
+  legacyMode: true,
 });
 
 //Redis 연결 성공
-redisClient.on('connect', () => {
-  console.info('✅ Redis connected!');
+redisClient.on("connect", () => {
+  console.info("✅ Redis connected!");
 });
 
 //Redis 연결 실패
-redisClient.on('error', (err) => {
-  console.error('❌ Redis Client Error', err);
-
+redisClient.on("error", (err) => {
+  console.error("❌ Redis Client Error", err);
 });
 
 //Redis v4 연결 (비동기)
-redisClient.connect().then(); 
-
+redisClient.connect().then();
 
 export const set = (key, value) => {
   redisClient.set(key, JSON.stringify(value));
@@ -39,7 +41,7 @@ export const get = (req, res, next) => {
       });
     }
     if (data !== null) {
-      console.log('data from redis!');
+      console.log("data from redis!");
       res.status(200).send({
         ok: true,
         data: JSON.parse(data),
@@ -52,11 +54,9 @@ export const get = (req, res, next) => {
 export const remove = (key) => {
   redisClient.del(key, (error, reply) => {
     if (error) {
-      console.error('캐시 삭제 실패:', error);
+      console.error("캐시 삭제 실패:", error);
     } else {
-      console.log('캐시 삭제 성공:', reply); // 삭제된 캐시 키의 수가 반환됩니다.
+      console.log("캐시 삭제 성공:", reply); // 삭제된 캐시 키의 수가 반환됩니다.
     }
   });
 };
-
-
