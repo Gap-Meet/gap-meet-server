7072ceba4444e6420762dcbe6a0ac7a0d80fe336
KJY <rlawldbs1007@gmail.com>
2023-11-07 13:39:17 +0900

    api파일추가-kjy

--- src/routes/group.js
+++ src/routes/group.js
@@ -3,6 +3,10 @@ import express from "express";
 // 각 API 엔드포인트에 대한 컨트롤러 함수 import(해당 엔드포인트에 대한 요청을 처리)
 import groupcreate from "../controller/group/groupCreate.js";
 import group_namecheck from "../controller/group/group_namecheck.js";
+import participation from "../controller/group/group_participation.js";
+import enter from "../controller/group/group_enter.js";
+import extract_groupname from "../controller/group/grouplist.js";
+
 //import { verifyJWT } from "../middlewares/tokenverify.js";
 
 export const groupRouter = express.Router();
@@ -18,4 +22,12 @@ groupRouter.post("/groupcreate", groupcreate);
 //GET /api/group/group_namecheck
 groupRouter.get("/group_namecheck", group_namecheck);
 
+//POST /api/group/participation
+groupRouter.post("/participation", participation);
+
+//GET /api/group/enter
+groupRouter.get("/enter", enter);
+
+//GET /api/group/grouplist
+groupRouter.get("/grouplist", extract_groupname);
 export default groupRouter;
